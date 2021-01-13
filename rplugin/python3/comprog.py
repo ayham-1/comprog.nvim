149c0b586e7240e8a87755fa207ab26786093491
altffour <altffour@protonmail.com>
2021-01-13 12:23:30 +0300

    add task interface

---
+++ rplugin/python3/comprog.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import pynvim
+
+@pynvim.plugin
+class Main(object):
+    def __init__(self, vim):
+        self.vim = vim
+
+    @pynvim.command('ComprogTaskCreate', nargs='*')
+    def createTask(self, args):
+        pass
+
+    @pynvim.command('ComprogTaskLoad', nargs='*')
+    def loadTask(self, args):
+        pass
+
+    @pynvim.command('ComprogTaskDel', nargs='*')
+    def delTask(self, args):
+        pass
+
+    @pynvim.command('ComprogTaskEdit', nargs='*')
+    def editTask(self, args):
+        pass
+
+    @pynvim.command('ComprogTaskLoadInput', nargs='*')
+    def loadInputTask(self, args):
+        pass
+
+    @pynvim.command('ComprogTaskCompile', nargs='*')
+    def compileTask(self, args):
+        pass
+
+    @pynvim.command('ComprogTaskRun', nargs='*')
+    def runTask(self, args):
+        pass
+
+    @pynvim.command('DoItPython')
+    def doItPython(self):
+        self.vim.command('echo "ello"')
