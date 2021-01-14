3607ea21634a7f7f3238dd9c6b49dee4e9ec3569
altffour <altffour@protonmail.com>
2021-01-14 12:34:54 +0300

    cprog: add run() func

--- rplugin/python3/lang_cpp.py
+++ rplugin/python3/lang_cpp.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-import os
+import subprocess
 from xdg import (xdg_cache_home)
 
 from data_types import Language
@@ -14,11 +14,19 @@ class Cplusplus(Language):
                                                 "cprog"))
     
     def compile(self, file_loc):
-        args = " {} {}".format(file_loc, self.args)
-        os.system("{} {}".format(self.cmd, args))
+        args = "{} {}".format(file_loc, self.args)
+        output = subprocess.check_output("{} {}".format(self.cmd, args),
+                                         shell=True)
+
+    def run(self):
+        cprog = os.path.join(xdg_cache_home(), "comprog", "cprog")
+        output = subprocess.check_output("exec {}".format(cprog),
+                                         shell=True)
+        return output
 
 if __name__ == "__main__":
+    import os
     os.system("mkdir -p {}".format(os.path.join(xdg_cache_home(), "comprog")))
     cpp = Cplusplus()
     cpp.compile("../../test/main.cpp")
-    os.system("{}".format(os.path.join(xdg_cache_home(), "comprog", "cprog")))
+    print(cpp.run())
